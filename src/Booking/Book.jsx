9ea2114fb1a16d946055b3743f0c021a93fe6503
Tinashe Muchineripi <muchineripitinashe28@gmail.com>
2025-04-23 14:14:46 +0100

    Add full booking component to help with routing to stripe, booking and checkout page

--- src/Booking/Book.jsx
+++ src/Booking/Book.jsx
@@ -1,24 +1,68 @@
-import React from 'react'
+import React, {useState}  from 'react'
+import { useLocation, NavLink } from 'react-router';
 
 function Book() {
+
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    
+    const {state} = useLocation();
+    console.log(state)
   return (
     <form action="">
         <label htmlFor="firstName">
-            First Name : <input type="text" id="firstName" name="firstName" />
+            First Name : <input 
+                            type="text" 
+                            id="firstName" 
+                            name="firstName"
+                            onChange={(e) => setFirstName(e.target.value)} 
+                        />
         </label>
         <label htmlFor="lastName">
-            Last Name : <input type="text" id="lastName" name="lastName" />
+            Last Name : <input 
+                            type="text" 
+                            id="lastName" 
+                            name="lastName" 
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
         </label>
         <label htmlFor="email">
-            Email : <input type="email" id="email" name="email" />
+            Email : <input 
+                        type="email" 
+                        id="email" 
+                        name="email" 
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
         </label>
         <label htmlFor="phone">
-            Phone : <input type="tel" id="phone" name="phone" />
+            Phone : <input 
+                        type="tel" 
+                        id="phone" 
+                        name="phone" 
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
         </label>
 
         {/* Add code for stripe integration here */}
+        <NavLink
+         to="stripe"
+         state={{
+            "startDate": state.startDate,
+            "endDate": state.endDate,
+            "roomTypeId": state.room,
+            "person": {
+                    "firstName": firstName,
+                    "lastName": lastName,
+                    "emailAddress": email,
+                    "phoneNo": phone
+                }
 
-        <button type="submit">Book</button>
+         }}
+        >
+            Continue to Payment
+        </NavLink>
     </form>
   )
 }
---
+++ src/Booking/FullBooking.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+function FullBooking() {
+  return (
+    <>
+        <Outlet />
+    </>
+  )
+}
+
+export default FullBooking
